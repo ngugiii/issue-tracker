df9948a07c9219a2ab2bbe47d658293848ff34d4
ngugiii <erickngugi54@gmail.com>
2023-12-29 13:15:36 +0300

    done with the dashboard

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,11 +1,13 @@
 "use client";
-import React, { useEffect,useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import BarChart from "../charts/Barchart";
 import axios from "axios";
 import classNames from "classnames";
+import Link from "next/link";
+import { AiOutlineArrowRight } from "react-icons/ai";
 
 const page = () => {
-    const [issues, setIssues] = useState([]);
+  const [issues, setIssues] = useState([]);
 
   const getIssues = async () => {
     try {
@@ -22,10 +24,10 @@ const page = () => {
 
   console.log(issues);
 
-  const today = new Date().toISOString().split('T')[0]; // Today's date in YYYY-MM-DD format
+  const today = new Date().toISOString().split("T")[0]; // Today's date in YYYY-MM-DD format
 
-  const todayIssues = issues.filter(issue => {
-    const issueDate = new Date(issue.createdAt).toISOString().split('T')[0]; // Issue's date in YYYY-MM-DD format
+  const todayIssues = issues.filter((issue) => {
+    const issueDate = new Date(issue.createdAt).toISOString().split("T")[0]; // Issue's date in YYYY-MM-DD format
     return issueDate === today; // Filter issues created today
   });
 
@@ -36,7 +38,7 @@ const page = () => {
     acc[status] = acc[status] ? acc[status] + 1 : 1;
     return acc;
   }, {});
-  
+
   console.log(groupedIssues);
 
   const chartRef = useRef(null);
@@ -44,19 +46,15 @@ const page = () => {
     labels: Object.keys(groupedIssues),
     datasets: [
       {
-        label: 'Issues',
+        label: "Issues",
         data: Object.values(groupedIssues),
-        backgroundColor: [
-          "#FF6347",
-          "#32CD32",
-          "#FFD700",
-        ],
+        backgroundColor: ["#FF6347", "#32CD32", "#FFD700"],
       },
     ],
   };
 
   const chartOptions = {
-    indexAxis: 'y',
+    indexAxis: "y",
     elements: {
       bar: {
         borderWidth: 1,
@@ -64,77 +62,86 @@ const page = () => {
     },
     responsive: true,
     plugins: {
-        title: {
-          display: true,
-          text: "Issue Status",
-        },
-        legend: {
-            display: false,
-          },
-        datalabels: {
-          color: '#ffffff',
-          anchor: 'end',
-          align: 'start',
-          offset: -10,
-          font: {
-            weight: 'bold',
-            size: '10',
-          },
-          formatter: (value) => value || '',
-        },
+      title: {
+        display: true,
+        text: "Issue Status",
       },
+      legend: {
+        display: false,
+      },
+      datalabels: {
+        color: "#ffffff",
+        anchor: "end",
+        align: "start",
+        offset: -10,
+        font: {
+          weight: "bold",
+          size: "10",
+        },
+        formatter: (value) => value || "",
+      },
+    },
     scales: {
-        x: {
-          grid: {
-            display: false,
-          },
-        },
-        y: {
-          grid: {
-            display: false,
-          },
+      x: {
+        grid: {
+          display: false,
         },
       },
+      y: {
+        grid: {
+          display: false,
+        },
+      },
+    },
   };
   return (
     <div className="px-24 flex w-full">
       <div className="w-full">
         <div className="flex justify-start mb-4">
-        {Object.keys(groupedIssues).map((key) => (
+          {Object.keys(groupedIssues).map((key) => (
             <div
               key={key}
               className="flex border-2 rounded-lg mr-4 p-1 border-gray-400 flex-col justify-center items-center"
             >
-              <span className="lowercase">{key} ISSUES</span>  <span>{groupedIssues[key]}</span>
+              <span className="lowercase">{key} ISSUES</span>{" "}
+              <span>{groupedIssues[key]}</span>
             </div>
           ))}
         </div>
         <div className=" p-4 shadow-lg rounded-md">
-          <BarChart chartOptions={chartOptions} chartData={chartData}/>
+          <BarChart chartOptions={chartOptions} chartData={chartData} />
         </div>
       </div>
-      <div className="border-2 ml-4 w-[50%] border-gray-300 p-4 rounded-lg">
-        <h1 className="text-xl flex items-center mb-2 text-[orangered]">Latest Issues<span className=" font-normal text-sm text-black ml-1"> (issues registered today)</span></h1>
-        {todayIssues.length>0 ? todayIssues.map((issue)=>(
-            <div className="mb-3 p-2 border-b">
-                <h1 className="mb-2">{issue.title}</h1>
-                <span
-                    className={classNames({
-                      "bg-red-100 text-red-600": issue.status === "OPEN",
-                      "bg-purple-100 text-purple-800":
-                        issue.status === "IN_PROGRESS",
-                      "bg-green-100 text-green-800": issue.status === "CLOSED",
-                      "p-1 rounded lowercase": true,
-                    })}
-                  >
-                    {issue.status}
-                  </span>
+      <div className="ml-8 w-[50%] shadow-lg p-4 rounded-lg">
+        <h1 className="text-xl flex items-center mb-2 text-[orangered]">
+          Latest Issues
+          <span className=" font-normal text-sm text-black ml-1">
+            {" "}
+            (issues registered today)
+          </span>
+        </h1>
+        {todayIssues.length > 0 ? (
+          todayIssues.map((issue) => (
+            <div className="mb-3 p-2 border-b flex flex-col">
+                <Link href={`issues/${issue.id}`} className=" hover:text-blue-700 hover:underline">
+            {issue.title}
+              </Link>
+              <span
+                className={classNames({
+                  "bg-red-100 text-red-600": issue.status === "OPEN",
+                  "bg-purple-100 text-purple-800":
+                    issue.status === "IN_PROGRESS",
+                  "bg-green-100 text-green-800": issue.status === "CLOSED",
+                  "p-1 rounded text-center w-[30%] mb-2 lowercase": true,
+                })}
+              >
+                {issue.status}
+              </span>
             </div>
-        )) : (
-            <div>No Issues Registered Today</div>
-        ) }
-
-
+          ))
+        ) : (
+          <div>No Issues Registered Today</div>
+        )}
       </div>
     </div>
   );
