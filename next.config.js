93574c8a8a51d81d74b65656daaf04518e410569
ngugiii <erickngugi54@gmail.com>
2023-12-31 15:35:59 +0300

    deploying

--- next.config.js
+++ next.config.js
@@ -1,14 +1,17 @@
-module.exports = {
-    webpack: (config, { isServer }) => {
-      if (!isServer) {
-        config.resolve.fallback = {
-          fs: false,
-          path: false,
-          os: false,
-          "@babel/runtime/regenerator": false, // Add this line
-        };
-      }
-      return config;
-    },
-  };
+// module.exports = {
+//     webpack: (config, { isServer }) => {
+//       if (!isServer) {
+//         config.resolve.fallback = {
+//           fs: false,
+//           path: false,
+//           os: false,
+//           "@babel/runtime/regenerator": false, // Add this line
+//         };
+//       }
+//       return config;
+//     },
+//   };
   
+module.exports = {
+    presets: ['next/babel'],
+  };
