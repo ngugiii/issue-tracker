5c0f2f867513bd9e4e92f493e68cac28e2da0133
ngugiii <erickngugi54@gmail.com>
2023-12-31 17:44:23 +0300

    changes

--- app/components/loader/Loader.jsx
+++ app/components/loader/Loader.jsx
@@ -1,11 +1,12 @@
 import styles from './Loader.module.scss';
 import loaderImg from '../../../public/loader.gif';
+import Image from 'next/image'
 
 const Loader = () => {
   return (
-    <div className="fixed top-0 left-0 flex justify-center items-center w-full h-full bg-black bg-opacity-80">
+    <div className="fixed z-10 top-0 left-0 flex justify-center items-center w-full h-full bg-black bg-opacity-80">
       <div className="text-center">
-      <img src="/loader.gif" alt="Loading..." className="w-12 h-12" />
+      <Image src="/loader.gif" alt="Loading..." className="w-12 h-12" width={3} height={3}/>
       </div>
     </div>
   );
